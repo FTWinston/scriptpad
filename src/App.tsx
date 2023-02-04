5c513dabab6390d411116d5dce2010f9ed197919
FTWinston <ftwinston@gmail.com>
2023-02-04 23:18:56 +0000

    Using system light/dark preferences

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useMemo } from 'react';
 import { Workspace as WorkspaceDisplay } from './layout/Workspace'
 import { Workspace } from './models/Workspace';
 
 const workspace = new Workspace(new Map());
 
 export const App: React.FC = () => {
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+    const theme = useMemo(
+        () => createTheme({
+            palette: {
+                mode: prefersDarkMode ? 'dark' : 'light',
+            },
+        }),
+        [prefersDarkMode],
+    );
+    
     return (
-        <WorkspaceDisplay workspace={workspace} />
+        <ThemeProvider theme={theme}>
+            <WorkspaceDisplay workspace={workspace} />
+        </ThemeProvider>
     )
 }
