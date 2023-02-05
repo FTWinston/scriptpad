46cf60c4531f26c9134711803ecb51c3e808b141
FTWinston <ftwinston@gmail.com>
2023-02-05 22:38:16 +0000

    Function library now has "add" button

--- src/layout/FunctionLibrary.tsx
+++ src/layout/FunctionLibrary.tsx
@@ -1,40 +1,39 @@
-import Box from '@mui/material/Box';
-import { FunctionId } from '../data/IFunction';
+import Box from '@mui/material/Box';;
+import AddIcon from '@mui/icons-material/Add';
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
-import ListItem from '@mui/material/ListItem';
+import { FunctionId } from '../data/IFunction'
 import { getNameColor } from '../services/getNameColor';
 import { getNameInitials } from '../services/getNameInitials';
 
 export interface Props {
     allFunctions: readonly FunctionId[];
     currentFunction: FunctionId | null;
-    selectFunction: (id: FunctionId) => void;
+    selectFunction: (id: FunctionId | null) => void;
 }
 
 export const FunctionLibrary: React.FC<Props> = props => {
-    const listItems = props.allFunctions.length === 0
-        ? (
-            <ListItem>
-                <ListItemText primary="Your function library is empty" secondary="Write a new function in the editor" />
-            </ListItem>
-        )
-        :  props.allFunctions.map(name => (
-                <ListItemButton key={name} selected={name === props.currentFunction} onClick={() => props.selectFunction(name)}>
-                    <ListItemAvatar>
-                        <Avatar sx={{ bgcolor: getNameColor(name) }}>{getNameInitials(name)}</Avatar>
-                    </ListItemAvatar>
-                    <ListItemText primary={name} />
-                </ListItemButton>
-            ));
-
     return (
         <Box>
             <List>
-                {listItems}
+                <ListItemButton selected={props.currentFunction === null} onClick={() => props.selectFunction(null)}>
+                    <ListItemAvatar>
+                        <Avatar sx={{ bgcolor: 'white' }}><AddIcon /></Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="Add new function" secondary="Write some code and run it immediately." />
+                </ListItemButton>
+
+                {props.allFunctions.map(name => (
+                    <ListItemButton key={name} selected={name === props.currentFunction} onClick={() => props.selectFunction(name)}>
+                        <ListItemAvatar>
+                            <Avatar sx={{ bgcolor: getNameColor(name) }}>{getNameInitials(name)}</Avatar>
+                        </ListItemAvatar>
+                        <ListItemText primary={name} />
+                    </ListItemButton>
+                ))}
             </List>
         </Box>
     );
