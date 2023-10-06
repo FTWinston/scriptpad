d7f230aee8ff29f04448be3390e917947aa04043
FTWinston <ftwinston@gmail.com>
2023-10-06 16:10:27 +0100

    Tweak icon bar background color

--- src/components/IconBar.tsx
+++ src/components/IconBar.tsx
@@ -1,10 +1,10 @@
 import Box from '@mui/material/Box';
-import { alpha, styled } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
 
 export const IconBar = styled(Box)(({ theme }) => ({
     position: 'absolute',
     right: 8,
     top: 8,
     borderRadius: 50,
-    backgroundColor: alpha(theme.palette.background.paper, 0.7),
+    backgroundColor: theme.palette.mode === 'dark' ? 'rgba(64,64,64,0.9)' : 'rgba(224,224,224,0.9)',
 }));
